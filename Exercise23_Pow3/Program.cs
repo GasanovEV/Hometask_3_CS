5e38a7b79fb29885e4afd5224aa18466939bcd2c
Egor Gasanov <Gasanov.EV11@ya.ru>
2023-06-23 00:10:10 +0300

    Exercise23_Pow3

---
+++ Exercise23_Pow3/Program.cs
@@ -0,0 +1,16 @@
+﻿﻿Console.Clear();
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine()??"");
+
+int count = 1;
+
+while (count<=number)
+{
+double Pow = Math.Pow(count,3);
+Console.Write ($"{Pow}, ");
+
+count = count+1;
+
+
+
+}
