bbfcf229862b6ea5690d42214313f9a5f7f0098d
Egor Gasanov <Gasanov.EV11@ya.ru>
2023-06-22 21:21:40 +0300

    Add Exercise19_Palindrome

---
+++ Exercise19_Palindrome/Program.cs
@@ -0,0 +1,33 @@
+﻿Console.Clear ();
+
+
+ int userInput = GetNumberFromUser("Введите целое число : ", "Ошибка ввода!");
+string result = PalindromeNumber(userInput);
+
+Console.WriteLine($"{userInput} -> {result}");
+
+//////////////////////////////
+int GetNumberFromUser(string message, string errorMessage)
+{
+    while (true)
+    {
+        Console.Write(message);
+        if (int.TryParse(Console.ReadLine(), out int userNumber)&&userNumber>9999&&userNumber<100000)
+            return userNumber;
+
+        Console.WriteLine(errorMessage);
+    }
+}
+string PalindromeNumber(int number)
+{
+    string palindrom ;
+   if ((number % 10 == number / 10000) && ((number / 1000) % 10 == (number % 100) / 10))
+    {
+        palindrom = "да";
+    }
+   else    { palindrom = "нет";} 
+    
+    return palindrom;
+}
+
+ 
